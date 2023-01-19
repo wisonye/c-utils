1734d132517f74ec55cbf40d7cfc48c61c261d69
Wison Ye <wisonye@gmail.com>
2023-01-19 19:27:28 +1300

    [ master ] - added handy data type mapping.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include "stdio.h"
 #include "utils/collections/single_link_list.h"
+#include "utils/data_types.h"
 
 //
 //
 //
 LL create_integer_list() {
-    size_t stack_value = 9999;
-    LL list = LL_from_value(sizeof(size_t), &stack_value, NULL);
+    usize stack_value = 9999;
+    LL list = LL_from_value(sizeof(usize), &stack_value, NULL);
     return list;
 }
 
@@ -24,17 +25,17 @@ int main(int argc, char **argv) {
     LL short_int_list = LL_from_empty();
 
     // Append a few nodes
-    size_t values[] = {111, 222, 333, 444, 555};
-    LL_append_value(&short_int_list, sizeof(uint16_t), &values[0], NULL);
-    LL_append_value(&short_int_list, sizeof(uint16_t), &values[1], NULL);
-    LL_append_value(&short_int_list, sizeof(uint16_t), &values[2], NULL);
-    LL_append_value(&short_int_list, sizeof(uint16_t), &values[3], NULL);
-    LL_append_value(&short_int_list, sizeof(uint16_t), &values[4], NULL);
+    usize values[] = {111, 222, 333, 444, 555};
+    LL_append_value(&short_int_list, sizeof(uint16), &values[0], NULL);
+    LL_append_value(&short_int_list, sizeof(uint16), &values[1], NULL);
+    LL_append_value(&short_int_list, sizeof(uint16), &values[2], NULL);
+    LL_append_value(&short_int_list, sizeof(uint16), &values[3], NULL);
+    LL_append_value(&short_int_list, sizeof(uint16), &values[4], NULL);
 
     // Get back the iter and check all data
     LLIterator *iter = LL_iter(&short_int_list);
-    for (size_t iter_index = 0; iter_index < iter->length; iter_index++) {
-        size_t temp_value = *((uint16_t *)iter->data_arr[iter_index]);
+    for (usize iter_index = 0; iter_index < iter->length; iter_index++) {
+        usize temp_value = *((uint16_t *)iter->data_arr[iter_index]);
         printf("\n>>>> temp_value: %lu", temp_value);
     }
     LL_free_iter(iter);
--- src/utils/data_types.h
+++ src/utils/data_types.h
@@ -8,9 +8,13 @@
 // Rust integer types
 //
 typedef uint8_t u8;
+typedef uint8_t uint8;
 typedef uint16_t u16;
+typedef uint16_t uint16;
 typedef uint32_t u32;
+typedef uint32_t uint32;
 typedef uint64_t u64;
+typedef uint64_t uint64;
 typedef size_t usize;
 
 //
