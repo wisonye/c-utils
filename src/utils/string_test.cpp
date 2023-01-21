e15b42bc6f25642434d7d88449de827ddfb0618d
Wison Ye <wisonye@gmail.com>
2023-01-21 17:48:48 +1300

    [ master ] - 'String/Str' updated unit test.

--- src/utils/string_test.cpp
+++ src/utils/string_test.cpp
@@ -36,3 +36,24 @@ TEST(String, StringFromArray) {
     ASSERT_EQ(Str_length(&str), 0);
     ASSERT_EQ(Str_as_str(&str), NULL);
 }
+
+TEST(String, StringClone) {
+    char arr[] = "Unit Test:)";
+    Str str = Str_from_arr(arr);
+    ASSERT_EQ(Str_length(&str), strlen(arr));
+    ASSERT_EQ(strcmp(Str_as_str(&str), arr), 0);
+
+    Str clone_1 = Str_clone(&str);
+    ASSERT_EQ(Str_length(&clone_1), strlen(arr));
+    ASSERT_EQ(strcmp(Str_as_str(&clone_1), arr), 0);
+
+    Str empty_str = Str_from_str("");
+    Str clone_from_empty = Str_clone(&empty_str);
+    ASSERT_EQ(Str_length(&clone_from_empty), 0);
+    ASSERT_EQ(Str_as_str(&clone_from_empty), NULL);
+    ASSERT_EQ(strcmp(Str_as_str(&clone_from_empty), arr), 1);
+
+    Str_free(&str);
+    Str_free(&clone_1);
+    Str_free(&empty_str);
+}
