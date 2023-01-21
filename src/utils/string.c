fe5130f93f7a0907cdb91a2732bc23625124d061
Wison Ye <wisonye@gmail.com>
2023-01-21 19:08:01 +1300

    [ master ] - 'String/Str' updated function doc.

--- src/utils/string.c
+++ src/utils/string.c
@@ -8,9 +8,9 @@
 #include <stdio.h>
 #endif
 
-//
-// Create from `char[]` or `char *`
-//
+/*
+ * Create from `char[]`
+ */
 Str Str_from_arr(const char arr[]) {
     usize temp_len = (arr != NULL) ? strlen(arr) : 0;
 
@@ -28,7 +28,9 @@ Str Str_from_arr(const char arr[]) {
     return string;
 }
 
-//
+/*
+ * Create from `char*`
+ */
 Str Str_from_str(const char *str) {
     usize temp_len = (str != NULL) ? strlen(str) : 0;
 
@@ -46,9 +48,9 @@ Str Str_from_str(const char *str) {
     return string;
 }
 
-//
-// Deep clone from `src`
-//
+/*
+ * Clone from given `Str` instance
+ */
 Str Str_clone(const Str *src) {
     String string = {
         ._len = 0,
@@ -65,18 +67,37 @@ Str Str_clone(const Str *src) {
     return string;
 }
 
-//
-// Getter
-//
+/*
+ * Append `char *` at the end
+ */
+void Str_append(const Str *self, const char *str_to_append) {}
+
+/*
+ * Append `char *` at the beginning
+ */
+void Str_insert_at_begin(const Str *self, const char *str_to_insert) {}
+
+/*
+ * Append `char *` at the given index
+ */
+void Str_insert_at_index(const Str *self, const char *str_to_insert,
+                         usize index_to_insert) {}
+
+/*
+ * Get back string length
+ */
 usize Str_length(const Str *self) { return (self != NULL) ? self->_len : 0; }
 
+/*
+ * Get back `char *`
+ */
 const char *Str_as_str(const Str *self) {
     return (self != NULL && self->_buffer != NULL) ? self->_buffer : NULL;
 }
 
-//
-// Query (not public)
-//
+/*
+ * Find implementation (not public)
+ */
 long Str_find_substring(const Str *self, const char *str_to_find,
                         bool case_sensitive) {
     if (self == NULL || self->_buffer == NULL || str_to_find == NULL ||
@@ -100,25 +121,28 @@ long Str_find_substring(const Str *self, const char *str_to_find,
                                                  : temp_ptr - self->_buffer;
 }
 
-//
-// Find the given `char *`, return `-1` if not found.
-//
+/*
+ * Find the given `char *` index, return `-1` if not found.
+ */
 long Str_index_of(const Str *self, const char *str_to_find) {
     return Str_find_substring(self, str_to_find, false);
 }
 
+/*
+ * Find the given `char *` (case sensitive) index, return `-1` if not found.
+ */
 long Str_index_of_case_sensitive(const Str *self, const char *str_to_find) {
     return Str_find_substring(self, str_to_find, true);
 }
 
-//
-// Check whether contain the given `char *`
-//
+/*
+ * Check whether contain the given `char *` or not
+ */
 bool Str_contains(const Str *self) {}
 
-//
-// Free
-//
+/*
+ * Free allocated memory, reset length to 0 and internal buffer to `NULL`
+ */
 void Str_free(Str *self) {
     if (self != NULL && self->_buffer != NULL) {
         free(self->_buffer);
--- src/utils/string.h
+++ src/utils/string.h
@@ -13,29 +13,66 @@ typedef struct {
     char *_buffer;
 } String, Str;
 
-//
-// Create from `char[]` or `char *`
-//
+/*
+ * Create from `char[]`
+ */
 Str Str_from_arr(const char arr[]);
+
+/*
+ * Create from `char*`
+ */
 Str Str_from_str(const char *str);
+
+/*
+ * Clone from given `Str` instance
+ */
 Str Str_clone(const Str *src);
 
-//
-// Getter
-//
+
+/*
+ * Append `char *` at the end
+ */
+void Str_append(const Str *self, const char *str_to_append);
+
+/*
+ * Append `char *` at the beginning
+ */
+void Str_insert_at_begin(const Str *self, const char *str_to_insert);
+
+/*
+ * Append `char *` at the given index
+ */
+void Str_insert_at_index(const Str *self, const char *str_to_insert,
+                         usize index_to_insert);
+
+/*
+ * Get back string length
+ */
 usize Str_length(const Str *self);
+
+/*
+ * Get back `char *`
+ */
 const char *Str_as_str(const Str *self);
 
-//
-// Query
-//
+/*
+ * Find the given `char *` index, return `-1` if not found.
+ */
 long Str_index_of(const Str *self, const char *str_to_find);
+
+/*
+ * Find the given `char *` (case sensitive) index, return `-1` if not found.
+ */
 long Str_index_of_case_sensitive(const Str *self, const char *str_to_find);
+
+/*
+ * Check whether contain the given `char *` or not
+ */
 bool Str_contains(const Str *self);
 
-//
-// Free
-//
+/*
+ * Free allocated memory, reset length to 0 and internal buffer to `NULL`
+ */
 void Str_free(Str *self);
 
 #endif
