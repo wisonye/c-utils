0b1fdd5f01f89f1253c92edf643de7e793ac79a6
Wison Ye <wisonye@gmail.com>
2023-01-23 18:43:40 +1300

    [ master ] - 'Log.c' changed all '%p' to uppercase address format.

--- src/utils/log.c
+++ src/utils/log.c
@@ -69,66 +69,80 @@ void print_const_string(char *v_name, const char *v) {
 // Print pointer
 //
 void print_void_ptr(char *v_name, void *v) {
-    printf("\n>>> %s (ptr): %p", v_name, v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR, v_name, (uintptr_t)v);
 }
 
 void print_bool_ptr(char *v_name, bool *v) {
-    printf("\n>>> %s (ptr): %p, value: %s", v_name, v,
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %s", v_name, (uintptr_t)v,
            (*v == true) ? "true" : "false");
 }
 
 void print_u8_ptr(char *v_name, u8 *v) {
-    printf("\n>>> %s (ptr): %p, value: %" PRIu8, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIu8, v_name,
+           (uintptr_t)v, *v);
 }
 
 void print_u16_ptr(char *v_name, u16 *v) {
-    printf("\n>>> %s (ptr): %p, value: %" PRIu16, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIu16, v_name,
+           (uintptr_t)v, *v);
 }
 
 void print_u32_ptr(char *v_name, u32 *v) {
-    printf("\n>>> %s (ptr): %p, value: %" PRIu32, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIu32, v_name,
+           (uintptr_t)v, *v);
 }
 
 void print_usize_ptr(char *v_name, usize *v) {
 #if __APPLE__
-    printf("\n>>> %s (ptr): %p, value: %" PRIuPTR, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIuPTR, v_name,
+           (uintptr_t)v, *v);
 #elif __FreeBSD__
-    printf("\n>>> %s (ptr): %p, value: %" PRIu64, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIu64, v_name,
+           (uintptr_t)v, *v);
 #else
-    printf("\n>>> %s (ptr): %p, value: %" PRIu64, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIu64, v_name,
+           (uintptr_t)v, *v);
 #endif
 }
 
 void print_u64_ptr(char *v_name, u64 *v) {
-    printf("\n>>> %s (ptr): %p, value: %" PRIu64, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIu64, v_name,
+           (uintptr_t)v, *v);
 }
 
 void print_i8_ptr(char *v_name, i8 *v) {
-    printf("\n>>> %s (ptr): %p, value: %" PRIi8, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIi8, v_name,
+           (uintptr_t)v, *v);
 }
 
 void print_i16_ptr(char *v_name, i16 *v) {
-    printf("\n>>> %s (ptr): %p, value: %" PRIi16, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIi16, v_name,
+           (uintptr_t)v, *v);
 }
 
 void print_i32_ptr(char *v_name, i32 *v) {
-    printf("\n>>> %s (ptr): %p, value: %" PRIi32, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIi32, v_name,
+           (uintptr_t)v, *v);
 }
 
 void print_i64_ptr(char *v_name, i64 *v) {
-    printf("\n>>> %s (ptr): %p, value: %" PRIi64, v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %" PRIi64, v_name,
+           (uintptr_t)v, *v);
 }
 
 void print_float_ptr(char *v_name, float *v) {
-    printf("\n>>> %s (ptr): %p, value: %f", v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %f", v_name, (uintptr_t)v,
+           *v);
 }
 
 void print_double_ptr(char *v_name, double *v) {
-    printf("\n>>> %s (ptr): %p, value: %f", v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %f", v_name, (uintptr_t)v,
+           *v);
 }
 
 void print_long_double_ptr(char *v_name, long double *v) {
-    printf("\n>>> %s (ptr): %p, value: %Lf", v_name, v, *v);
+    printf("\n>>> %s (ptr): 0x%" PRIXPTR ", value: %Lf", v_name, (uintptr_t)v,
+           *v);
 }
 
 /**
