74abbc2480195a1b0850c4a744c3e8aea5fea287
Wison Ye <wisonye@gmail.com>
2023-02-11 16:13:10 +1300

    [ master ] - Fixed 'vector' unit test.

--- src/utils/collections/vector_test.cpp
+++ src/utils/collections/vector_test.cpp
@@ -1,27 +1,26 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-// #include "string.h"
 #include <string.h>
 
 #include "vector.h"
 }
 
 TEST(Vector, EmptyVector) {
-    SMART_VECTOR(empty_vec) = Vector_new();
-    ASSERT_EQ(Vector_len(empty_vec), 0);
-    ASSERT_EQ(Vector_capacity(empty_vec), 0);
-    VectorIteractor iter = Vector_iter(empty_vec);
+    SMART_VECTOR(empty_vec) = Vec_new(sizeof(int));
+    ASSERT_EQ(Vec_len(empty_vec), 0);
+    ASSERT_EQ(Vec_capacity(empty_vec), 0);
+    VectorIteractor iter = Vec_iter(empty_vec);
     ASSERT_EQ(iter.length, 0);
     ASSERT_EQ(iter.items, NULL);
 }
 
 TEST(Vector, EmptyVectorWithCapiticy) {
     usize u16_type_size = sizeof(u16);
-    SMART_VECTOR(vec) = Vector_with_capacity(10, u16_type_size);
-    ASSERT_EQ(Vector_len(vec), 0);
-    ASSERT_EQ(Vector_capacity(vec), 10);
-    VectorIteractor iter_2 = Vector_iter(vec);
+    SMART_VECTOR(vec) = Vec_with_capacity(10, u16_type_size);
+    ASSERT_EQ(Vec_len(vec), 0);
+    ASSERT_EQ(Vec_capacity(vec), 10);
+    VectorIteractor iter_2 = Vec_iter(vec);
     ASSERT_EQ(iter_2.length, 0);
     ASSERT_NE(iter_2.items, NULL);
 
@@ -31,26 +30,26 @@ TEST(Vector, EmptyVectorWithCapiticy) {
     //
     u16 short_arr[] = {5000, 5001, 5002, 5003, 5004, 5005,
                        5006, 5007, 5008, 5009, 6000};
-    Vector_push(vec, &short_arr[0], u16_type_size);
-    Vector_push(vec, &short_arr[1], u16_type_size);
-    Vector_push(vec, &short_arr[2], u16_type_size);
-    Vector_push(vec, &short_arr[3], u16_type_size);
-    Vector_push(vec, &short_arr[4], u16_type_size);
-    Vector_push(vec, &short_arr[5], u16_type_size);
-    Vector_push(vec, &short_arr[6], u16_type_size);
-    Vector_push(vec, &short_arr[7], u16_type_size);
-    Vector_push(vec, &short_arr[8], u16_type_size);
-    Vector_push(vec, &short_arr[9], u16_type_size);
-    ASSERT_EQ(Vector_len(vec), 10);
-    ASSERT_EQ(Vector_capacity(vec), 10);
+    Vec_push(vec, &short_arr[0]);
+    Vec_push(vec, &short_arr[1]);
+    Vec_push(vec, &short_arr[2]);
+    Vec_push(vec, &short_arr[3]);
+    Vec_push(vec, &short_arr[4]);
+    Vec_push(vec, &short_arr[5]);
+    Vec_push(vec, &short_arr[6]);
+    Vec_push(vec, &short_arr[7]);
+    Vec_push(vec, &short_arr[8]);
+    Vec_push(vec, &short_arr[9]);
+    ASSERT_EQ(Vec_len(vec), 10);
+    ASSERT_EQ(Vec_capacity(vec), 10);
 
     // `capacity` should change to `20`
-    Vector_push(vec, &short_arr[10], u16_type_size);
-    ASSERT_EQ(Vector_len(vec), 11);
-    ASSERT_EQ(Vector_capacity(vec), 20);
+    Vec_push(vec, &short_arr[10]);
+    ASSERT_EQ(Vec_len(vec), 11);
+    ASSERT_EQ(Vec_capacity(vec), 20);
 
     // Element value check
-    VectorIteractor iter = Vector_iter(vec);
+    VectorIteractor iter = Vec_iter(vec);
     ASSERT_EQ(iter.length, 11);
     u16 *temp_arr = (u16 *)iter.items;
     ASSERT_EQ(temp_arr[0], short_arr[0]);
@@ -68,19 +67,19 @@ TEST(Vector, EmptyVectorWithCapiticy) {
 
 TEST(Vector, PushElement_Integer) {
     int int_arr[] = {100, 200, 300};
-    SMART_VECTOR(vec) = Vector_new();
-    Vector_push(vec, &int_arr[0], sizeof(int));
-    ASSERT_EQ(Vector_len(vec), 1);
-    ASSERT_EQ(Vector_capacity(vec), 1);
-    Vector_push(vec, &int_arr[1], sizeof(int));
-    ASSERT_EQ(Vector_len(vec), 2);
-    ASSERT_EQ(Vector_capacity(vec), 2);
-    Vector_push(vec, &int_arr[2], sizeof(int));
-    ASSERT_EQ(Vector_len(vec), 3);
-    ASSERT_EQ(Vector_capacity(vec), 4);
+    SMART_VECTOR(vec) = Vec_new(sizeof(int));
+    Vec_push(vec, &int_arr[0]);
+    ASSERT_EQ(Vec_len(vec), 1);
+    ASSERT_EQ(Vec_capacity(vec), 1);
+    Vec_push(vec, &int_arr[1]);
+    ASSERT_EQ(Vec_len(vec), 2);
+    ASSERT_EQ(Vec_capacity(vec), 2);
+    Vec_push(vec, &int_arr[2]);
+    ASSERT_EQ(Vec_len(vec), 3);
+    ASSERT_EQ(Vec_capacity(vec), 4);
 
-    VectorIteractor iter = Vector_iter(vec);
-    ASSERT_EQ(iter.length, Vector_len(vec));
+    VectorIteractor iter = Vec_iter(vec);
+    ASSERT_EQ(iter.length, Vec_len(vec));
     int *temp_arr = (int *)iter.items;
     ASSERT_NE(temp_arr, NULL);
     ASSERT_EQ(temp_arr[0], 100);
@@ -88,27 +87,27 @@ TEST(Vector, PushElement_Integer) {
     ASSERT_EQ(temp_arr[2], 300);
 
     int int_arr_2[] = {666, 777, 888, 999};
-    SMART_VECTOR(vec_2) = Vector_new();
-    Vector_push(vec_2, &int_arr_2[0], sizeof(int));
-    ASSERT_EQ(Vector_len(vec_2), 1);
-    ASSERT_EQ(Vector_capacity(vec_2), 1);
-    Vector_push(vec_2, &int_arr_2[1], sizeof(int));
-    ASSERT_EQ(Vector_len(vec_2), 2);
-    ASSERT_EQ(Vector_capacity(vec_2), 2);
-    Vector_push(vec_2, &int_arr_2[2], sizeof(int));
-    ASSERT_EQ(Vector_len(vec_2), 3);
-    ASSERT_EQ(Vector_capacity(vec_2), 4);
+    SMART_VECTOR(vec_2) = Vec_new(sizeof(int));
+    Vec_push(vec_2, &int_arr_2[0]);
+    ASSERT_EQ(Vec_len(vec_2), 1);
+    ASSERT_EQ(Vec_capacity(vec_2), 1);
+    Vec_push(vec_2, &int_arr_2[1]);
+    ASSERT_EQ(Vec_len(vec_2), 2);
+    ASSERT_EQ(Vec_capacity(vec_2), 2);
+    Vec_push(vec_2, &int_arr_2[2]);
+    ASSERT_EQ(Vec_len(vec_2), 3);
+    ASSERT_EQ(Vec_capacity(vec_2), 4);
 
     //
     // already be able to hold 4 elements. so `capacity` no needs to change,
     // no `realloc` to be called. */
     //
-    Vector_push(vec_2, &int_arr_2[3], sizeof(int));
-    ASSERT_EQ(Vector_len(vec_2), 4);
-    ASSERT_EQ(Vector_capacity(vec_2), 4);
+    Vec_push(vec_2, &int_arr_2[3]);
+    ASSERT_EQ(Vec_len(vec_2), 4);
+    ASSERT_EQ(Vec_capacity(vec_2), 4);
 
-    VectorIteractor iter_2 = Vector_iter(vec_2);
-    ASSERT_EQ(iter_2.length, Vector_len(vec_2));
+    VectorIteractor iter_2 = Vec_iter(vec_2);
+    ASSERT_EQ(iter_2.length, Vec_len(vec_2));
     int *temp_arr_2 = (int *)iter_2.items;
     ASSERT_NE(temp_arr_2, NULL);
     ASSERT_EQ(temp_arr_2[0], 666);
@@ -128,19 +127,19 @@ TEST(Vector, PushElement_custom_struct) {
     Person fion = {.first_name = "Fion", .last_name = "Li", .age = 99};
     Person nobody = {
         .first_name = "Nobody", .last_name = "Nothing", .age = 100};
-    SMART_VECTOR(vec) = Vector_new();
-    Vector_push(vec, &wison, sizeof(Person));
-    ASSERT_EQ(Vector_len(vec), 1);
-    ASSERT_EQ(Vector_capacity(vec), 1);
-    Vector_push(vec, &fion, sizeof(Person));
-    ASSERT_EQ(Vector_len(vec), 2);
-    ASSERT_EQ(Vector_capacity(vec), 2);
-    Vector_push(vec, &nobody, sizeof(Person));
-    ASSERT_EQ(Vector_len(vec), 3);
-    ASSERT_EQ(Vector_capacity(vec), 4);
+    SMART_VECTOR(vec) = Vec_new(sizeof(Person));
+    Vec_push(vec, &wison);
+    ASSERT_EQ(Vec_len(vec), 1);
+    ASSERT_EQ(Vec_capacity(vec), 1);
+    Vec_push(vec, &fion);
+    ASSERT_EQ(Vec_len(vec), 2);
+    ASSERT_EQ(Vec_capacity(vec), 2);
+    Vec_push(vec, &nobody);
+    ASSERT_EQ(Vec_len(vec), 3);
+    ASSERT_EQ(Vec_capacity(vec), 4);
 
-    VectorIteractor iter = Vector_iter(vec);
-    ASSERT_EQ(iter.length, Vector_len(vec));
+    VectorIteractor iter = Vec_iter(vec);
+    ASSERT_EQ(iter.length, Vec_len(vec));
     Person *temp_arr = (Person *)iter.items;
     ASSERT_NE(temp_arr, NULL);
     ASSERT_EQ(strcmp(temp_arr[0].first_name, "Wison"), 0);
@@ -168,40 +167,33 @@ usize person_arr_len = sizeof(person_arr) / sizeof(person_arr[0]);
 
 TEST(Vector, immutable_get) {
     SMART_VECTOR(double_vec) =
-        Vector_with_capacity(double_arr_len, double_type_size);
+        Vec_with_capacity(double_arr_len, double_type_size);
     for (usize di = 0; di < double_arr_len; di++) {
-        Vector_push(double_vec, &double_arr[di], double_type_size);
+        Vec_push(double_vec, &double_arr[di]);
     }
-    ASSERT_EQ(Vector_len(double_vec), double_arr_len);
-    ASSERT_EQ(Vector_capacity(double_vec), double_arr_len);
+    ASSERT_EQ(Vec_len(double_vec), double_arr_len);
+    ASSERT_EQ(Vec_capacity(double_vec), double_arr_len);
 
-    const double *d_value_1 =
-        (const double *)Vector_get(double_vec, 0, double_type_size);
-    const double *d_value_2 =
-        (const double *)Vector_get(double_vec, 1, double_type_size);
-    const double *d_value_3 =
-        (const double *)Vector_get(double_vec, 2, double_type_size);
+    const double *d_value_1 = (const double *)Vec_get(double_vec, 0);
+    const double *d_value_2 = (const double *)Vec_get(double_vec, 1);
+    const double *d_value_3 = (const double *)Vec_get(double_vec, 2);
     ASSERT_EQ(*d_value_1, double_arr[0]);
     ASSERT_EQ(*d_value_2, double_arr[1]);
     ASSERT_EQ(*d_value_3, double_arr[2]);
-    const double *d_value_4 =
-        (const double *)Vector_get(double_vec, 3, double_type_size);
+    const double *d_value_4 = (const double *)Vec_get(double_vec, 3);
     ASSERT_EQ(d_value_4, NULL);
 
     SMART_VECTOR(person_vec) =
-        Vector_with_capacity(person_arr_len, person_type_size);
+        Vec_with_capacity(person_arr_len, person_type_size);
     for (usize pi = 0; pi < double_arr_len; pi++) {
-        Vector_push(person_vec, &person_arr[pi], person_type_size);
+        Vec_push(person_vec, &person_arr[pi]);
     }
-    ASSERT_EQ(Vector_len(person_vec), person_arr_len);
-    ASSERT_EQ(Vector_capacity(person_vec), person_arr_len);
+    ASSERT_EQ(Vec_len(person_vec), person_arr_len);
+    ASSERT_EQ(Vec_capacity(person_vec), person_arr_len);
 
-    const Person *person_1 =
-        (const Person *)Vector_get(person_vec, 0, person_type_size);
-    const Person *person_2 =
-        (const Person *)Vector_get(person_vec, 1, person_type_size);
-    const Person *person_3 =
-        (const Person *)Vector_get(person_vec, 2, person_type_size);
+    const Person *person_1 = (const Person *)Vec_get(person_vec, 0);
+    const Person *person_2 = (const Person *)Vec_get(person_vec, 1);
+    const Person *person_3 = (const Person *)Vec_get(person_vec, 2);
 
     ASSERT_EQ(strcmp(person_1->first_name, person_arr[0].first_name), 0);
     ASSERT_EQ(strcmp(person_1->last_name, person_arr[0].last_name), 0);
@@ -213,7 +205,6 @@ TEST(Vector, immutable_get) {
     ASSERT_EQ(strcmp(person_3->last_name, person_arr[2].last_name), 0);
     ASSERT_EQ(person_3->age, person_arr[2].age);
 
-    const Person *person_4 =
-        (const Person *)Vector_get(person_vec, 3, person_type_size);
+    const Person *person_4 = (const Person *)Vec_get(person_vec, 3);
     ASSERT_EQ(person_4, NULL);
 }
