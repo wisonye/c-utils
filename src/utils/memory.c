0f793ef3c8f80fae37737b9b18630e958b8b628b
Wison Ye <wisonye@gmail.com>
2023-02-15 13:24:57 +1300

    [ master ] - 'Memory' fixed 'memory_bytes' doesn't initialise and add important comment.

--- src/utils/memory.c
+++ src/utils/memory.c
@@ -1,5 +1,7 @@
 #include "memory.h"
 
+#include <string.h>
+
 #include "log.h"
 
 /*
@@ -23,16 +25,45 @@ void print_memory_block(char *type_name, usize type_size, void *value) {
         DEBUG_LOG(Memory, print_memory_block, "%s\n", hr);
     } else {
         u8 memory_byte = 0x00;
-        char memory_bytes[type_size * 2 + 1];
+        usize memory_bytes_len = hex_len_in_char + 1;
+        char memory_bytes[memory_bytes_len];
+        memset(memory_bytes, 0, memory_bytes_len);
+
         for (usize index = 0; index < type_size; index++) {
             // 1. Convert (void *) to `(unsigned char *)`, so each `+1` means
-            // moving
-            //    just 1 byte!!!
-            // 2. Get back the pointer offset by `(type_name *) + index`
+            //    moving just 1 byte!!!
+            // 2. Get back the pointer offset by `(u8 *) + index`
             // 3. Dereference that pointer offset value (in HEX format)
             memory_byte = *(((u8 *)value) + index);
 
-            char hex_buffer[2];
+            //
+            // If you run `valgrind --leak-check=full --show-reachable=yes -s
+            // --track-origins=yes`, it will print out the following error on
+            // the `sprintf` line:
+            //
+            // ==80526== Use of uninitialised value of size 8
+            // ==80526==    at 0x497EE65: ??? (in /lib/libc.so.7)
+            // ==80526==    by 0x498846D: vsprintf (in /lib/libc.so.7)
+            // ==80526==    by 0x497BACC: sprintf (in /lib/libc.so.7)
+            // ==80526==    by 0x2081A8: print_memory_block
+            // (src/utils/memory.c:44)
+            // ==80526==    by 0x20744F: Str_push_str (src/utils/string.c:290)
+            // ==80526==    by 0x20C913: main (src/main.c:758)
+            // ==80526==  Uninitialised value was created by a heap allocation
+            // ==80526==    at 0x484C984: malloc (in
+            // /usr/local/libexec/valgrind/vgpreload_memcheck-amd64-freebsd.so)
+            // ==80526==    by 0x2073EE: Str_push_str (src/utils/string.c:278)
+            // ==80526==    by 0x20C913: main (src/main.c:758)
+            //
+            //
+            // It's a little bit confuses, as it complains about the `value`
+            // is uninitialised, as that's the `self->_buffer` and it hasn't
+            // been initialised after:
+            //
+            // `realloc(self->_buffer, new_len_with_null_terminated_char);`
+            //
+            // That's what it complains about, you can totally just ignore it:)
+            //
             sprintf(memory_bytes + (index * 2), "%02X", memory_byte);
         }
         memory_bytes[hex_len_in_char] = '\0';
