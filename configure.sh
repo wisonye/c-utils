05730a79585cdb5ee69c79f448f6c059f3fd6dae
Wison Ye <wisonye@gmail.com>
2023-10-17 16:54:21 +1300

    [ master ] - Added 'configure.sh' for quick install to '~/my-install' and updated README about it.

---
+++ configure.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+OS_TYPE=`uname -s`
+echo ">>> OS_TYPE: ${OS_TYPE}"
+
+INSTALL_PREFIX="/home/${USER}/my-installed"
+
+if [ "${OS_TYPE}" = "Darwin" ]; then
+    INSTALL_PREFIX="/Users/${USER}/my-installed"
+fi
+echo ">>> INSTALL_PREFIX: ${INSTALL_PREFIX}"
+
+#
+# Remove everything exists
+#
+./remove_all_cmake_build_folders.sh
+
+#
+# Run cmake to generate all files
+#
+cmake -S ./cmake -B ./temp_build/build_memory_leak_checking \
+    -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
+
+#
+# Copy `compile_commands.json` to `build/compile_commands.json` for neovim LSP
+#
+mkdir build
+cp -rvf ./temp_build/build_memory_leak_checking/compile_commands.json ./build
