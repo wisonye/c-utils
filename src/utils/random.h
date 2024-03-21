ac96905b069bdcddb526f8bca0c31f900edf7180
Wison Ye <wisonye@gmail.com>
2024-03-22 12:54:14 +1300

    [ master ] - Fixed include guard for random util.

--- src/utils/random.h
+++ src/utils/random.h
@@ -1,5 +1,8 @@
+#ifndef __RANDOM_NUMBERS_H__
+#define __RANDOM_NUMBERS_H__
 #include "data_types.h"
 
 float get_random_float_between_0_and_1(void);
 float get_random_float(float max_float);
 u32 get_random_uint(u32 max_uint);
+#endif
