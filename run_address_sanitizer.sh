baee613059d74dff2d67d379c5b450506f4760b6
Wison Ye <wisonye@gmail.com>
2024-03-31 17:23:03 +1300

    cmake/unit_test/CMakeLists.txt

--- run_address_sanitizer.sh
+++ run_address_sanitizer.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-cd temp_build/build_memory_leak_checking; clear; make && ASAN_OPTIONS=detect_leaks=1 ./c-utils-demo; cd ..
+OS_NAME=`uname -s`
+echo "OS_NAME: ${OS_NAME}"
+if [ "${OS_NAME}" == "FreeBSD" ]; then
+    cd temp_build/build_memory_leak_checking; clear; make && ./c-utils-demo; cd ..
+else
+    cd temp_build/build_memory_leak_checking; clear; make && ASAN_OPTIONS=detect_leaks=1 ./c-utils-demo; cd ..
+fi
--- run_address_sanitizer_freebsd.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd temp_build/build_memory_leak_checking; clear; make && ./c-utils-demo; cd ..
